e7cd07fdc63b5ad08ab5dccdbe1a9e987f6af08b
sundarammishra2006-git <sundarammishra2006@gmail.com>
2026-04-10 10:17:34 +0530

    Add files via upload

---
+++ p6.js
@@ -0,0 +1,6 @@
+let a = "hello world";
+let b = a.toUpperCase();
+let c = a.toLowerCase();
+console.log(b);
+console.log(c); // Output: "HELLO WORLD" and "hello world"
+console.log(a.concat(" ", b)); // Output: "hello worldThis text b"
