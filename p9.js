dcc942ce5e221051f527e3e0698c73ae9cdb9904
sundarammishra2006-git <sundarammishra2006@gmail.com>
2026-04-11 10:29:57 +0530

    Add files via upload

---
+++ p9.js
@@ -0,0 +1,4 @@
+let splt = "Hello";
+let myArr = console.log(splt.split("")); // Output: ["Hello,", "World!"]
+let priority = "Hello World";
+let char = console.log(priority[-1]); // Output: undefined
