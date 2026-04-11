4e12ad5a09292e4429eb590072d44b88d6c270e7
sundarammishra2006-git <sundarammishra2006@gmail.com>
2026-04-11 13:46:49 +0530

    Add files via upload

---
+++ p11.js
@@ -0,0 +1,10 @@
+let splt = "Hello";
+console.log(splt.split(""));
+let priority = "HELLO WORLD";
+let char= console.log(priority[-1]);
+name= "SUNDARAM MISHRA"
+console.log(name.indexOf("A"))
+console.log(name.lastIndexOf("A"))
+console.log(name.startsWith("S"))
+console.log(name.endsWith("A"))
+console.log(name.search("I"))
